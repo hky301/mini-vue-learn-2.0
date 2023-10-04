36be956cdc32f30bf306a7fc947960c3eff9dcfa
HeKaiYu <hekaiyu_301@163.com>
2023-10-04 16:11:28 +0800

    feat: 07-实现 effect 返回 runner

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -6,7 +6,7 @@ class ReactiveEffect {
   }
   run() {
     activeEffect = this
-    this._fn()
+    return this._fn()
   }
 }
 
@@ -14,6 +14,8 @@ export function effect(fn) {
   const _effect = new ReactiveEffect(fn)
 
   _effect.run()
+
+  return _effect.run.bind(_effect)
 }
 
 let activeEffect
