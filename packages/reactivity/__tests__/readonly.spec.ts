d0b4ae8e256e1c5d2b2312d0a30a3a3e48c0337f
HeKaiYu <hekaiyu_301@163.com>
2023-10-06 15:18:31 +0800

    feat: 15-实现 isProxy 功能

--- packages/reactivity/__tests__/readonly.spec.ts
+++ packages/reactivity/__tests__/readonly.spec.ts
@@ -1,5 +1,5 @@
 
-import { isReadonly, readonly } from '../src/reactive'
+import { isProxy, isReadonly, readonly } from '../src/reactive'
 import { vi } from 'vitest'
 
 describe('readonly', () => {
@@ -13,6 +13,7 @@ describe('readonly', () => {
     expect(isReadonly(original)).toBe(false)
     expect(isReadonly(wrapped.bar)).toBe(true)
     expect(isReadonly(original.bar)).toBe(false)
+    expect(isProxy(wrapped)).toBe(true)
   })
 
   it('warn then call set', () => {
