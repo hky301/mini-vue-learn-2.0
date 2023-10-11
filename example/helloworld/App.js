e42635520a8e01a0ecd5f987188d8a83672809a4
HeKaiYu <hekaiyu_301@163.com>
2023-10-11 16:04:01 +0800

    feat: 22-实现初始化 element 主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -3,7 +3,12 @@ import { h } from '../../packages/vue/dist/guide-mini-vue.esm.js';
 export const App = {
   name: 'App',
   render() {
-    return h('div', "hi," + this.msg)
+    return h('div', {
+      id: 'root',
+      class: ['red', 'blue']
+    }, [
+      h('p', { class: 'red' }, 'hi,'), h('p', { class: 'blue' }, 'mini-vue')
+    ])
   },
   setup() {
     return {
--- packages/vue/dist/guide-mini-vue.cjs.js
+++ packages/vue/dist/guide-mini-vue.cjs.js
@@ -1,5 +1,13 @@
 'use strict';
 
+function isObject(target) {
+    return typeof target === 'object' && target !== null;
+}
+const extend = Object.assign;
+const hasChanged = (val, newValue) => {
+    return !Object.is(val, newValue);
+};
+
 function createComponentInstance(vnode) {
     const component = {
         vnode,
@@ -35,25 +43,54 @@ function finishComponentSetup(instance) {
 }
 
 function render(vnode, container) {
-    patch(vnode);
+    patch(vnode, container);
 }
 function patch(vnode, container) {
-    processComponent(vnode);
+    // 区分是element 还是 component 类型
+    if (typeof vnode.type === 'string') {
+        processElement(vnode, container);
+    }
+    else if (isObject(vnode.type)) {
+        processComponent(vnode, container);
+    }
+}
+function processElement(vnode, container) {
+    mountElement(vnode, container);
+}
+function mountElement(vnode, container) {
+    const el = document.createElement(vnode.type);
+    const { children, props } = vnode;
+    if (typeof children === 'string') {
+        el.textContent = children;
+    }
+    else if (Array.isArray(children)) {
+        mountChildren(vnode, el);
+    }
+    for (const key in props) {
+        const val = props[key];
+        el.setAttribute(key, val);
+    }
+    container.append(el);
+}
+function mountChildren(vnode, container) {
+    vnode.children.forEach(v => {
+        patch(v, container);
+    });
 }
 function processComponent(vnode, container) {
-    mountComponent(vnode);
+    mountComponent(vnode, container);
 }
 function mountComponent(vnode, container) {
     // 创建组件实例
     const instance = createComponentInstance(vnode);
     // 处理setup函数
     setupComponent(instance);
-    setupRenderEffect(instance);
+    setupRenderEffect(instance, container);
 }
 function setupRenderEffect(instance, container) {
     const subTree = instance.render();
     // patch
-    patch(subTree);
+    patch(subTree, container);
 }
 
 function createVNode(type, props, children) {
@@ -69,7 +106,7 @@ function createApp(rootComponent) {
     return {
         mount(rootContainer) {
             const vnode = createVNode(rootComponent);
-            render(vnode);
+            render(vnode, rootContainer);
         }
     };
 }
@@ -77,14 +114,6 @@ function createApp(rootComponent) {
 function h(type, props, children) {
     return createVNode(type, props, children);
 }
-
-function isObject(target) {
-    return typeof target === 'object' && target !== null;
-}
-const extend = Object.assign;
-const hasChanged = (val, newValue) => {
-    return !Object.is(val, newValue);
-};
 
 let activeEffect;
 let shouldTrack = false;
