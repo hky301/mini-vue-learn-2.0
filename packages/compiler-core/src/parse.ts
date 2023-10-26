41085d5f527481656b16522f71578565758d46f7
HeKaiYu <hekaiyu_301@163.com>
2023-10-26 10:44:55 +0800

    feat: 46-实现解析三种联合类型

--- packages/compiler-core/src/parse.ts
+++ packages/compiler-core/src/parse.ts
@@ -9,31 +9,59 @@ const enum TagType {
 export function baseParse(content: string) {
   const context = createParserContext(content)
 
-  return createRoot(parseChildren(context))
+  return createRoot(parseChildren(context, []))
 }
 
-function parseChildren(context) {
+function parseChildren(context, ancestors) {
   const nodes: any[] = []
-  let node
-  const s = context.source
-  if (s.startsWith('{{')) {
-    node = parseInterpolation(context)
-  } else if (s[0] === '<') {
-    if (/[a-z]/i.test(s[1])) {
-      node = parseElement(context)
+
+  while (!isEnd(context, ancestors)) {
+    let node
+    const s = context.source
+    if (s.startsWith('{{')) {
+      node = parseInterpolation(context)
+    } else if (s[0] === '<') {
+      if (/[a-z]/i.test(s[1])) {
+        node = parseElement(context, ancestors)
+      }
     }
-  }
 
-  if (!node) {
-    node = parseText(context)
-  }
+    if (!node) {
+      node = parseText(context)
+    }
 
-  nodes.push(node)
+    nodes.push(node)
+  }
   return nodes
 }
 
+function isEnd(context, ancestors) {
+  // 1.source 有值的时候
+  // 2.当遇到结束标签的时候
+  const s = context.source
+
+  if (s.startsWith('</')) {
+    for (let i = ancestors.length - 1; i >= 0; i--) {
+      const tag = ancestors[i].tag
+      if (startsWithEndTagOpen(s, tag)) {
+        return true
+      }
+    }
+  }
+  return !s
+}
+
 function parseText(context: any): any {
-  const content = parseTextData(context, context.source.length)
+  let endIndex = context.source.length
+  let endToken = ['<', '{{']
+  for (let i = 0; i < endToken.length; i++) {
+    const index = context.source.indexOf(endToken[i])
+    if (index !== -1 && endIndex > index) {
+      endIndex = index
+    }
+  }
+
+  const content = parseTextData(context, endIndex)
 
   return {
     type: NodeTypes.TEXT,
@@ -50,13 +78,24 @@ function parseTextData(context: any, length: number) {
   return content
 }
 
-function parseElement(context) {
-
-  const element = parseTag(context, TagType.Start)
-  parseTag(context, TagType.End)
+function parseElement(context, ancestors) {
+  const element: any = parseTag(context, TagType.Start)
+  ancestors.push(element)
+  element.children = parseChildren(context, ancestors)
+  ancestors.pop()
+  if (startsWithEndTagOpen(context.source, element.tag)) {
+    parseTag(context, TagType.End)
+  } else {
+    throw new Error(`缺少结束标签:${element.tag}`)
+  }
 
   return element
 }
+
+function startsWithEndTagOpen(source, tag) {
+  return source.startsWith('</') && source.slice(2, 2 + tag.length).toLowerCase() === tag.toLowerCase()
+}
+
 
 function parseTag(context: any, type: TagType) {
   const match: any = /^<\/?([a-z]*)/i.exec(context.source)
