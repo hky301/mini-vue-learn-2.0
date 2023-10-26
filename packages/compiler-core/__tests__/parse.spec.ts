988bb594cd7a2648809afe4b005c0a600d63efb2
HeKaiYu <hekaiyu_301@163.com>
2023-10-26 08:54:27 +0800

    feat: 44-实现解析 element

--- packages/compiler-core/__tests__/parse.spec.ts
+++ packages/compiler-core/__tests__/parse.spec.ts
@@ -16,6 +16,17 @@ describe('Parse', () => {
       })
     })
   })
+
+  describe('element', () => {
+    test('simple element div', () => {
+      const ast = baseParse("<div></div>")
+
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.ELEMENT,
+        tag: 'div'
+      })
+    })
+  })
 })
 
 
